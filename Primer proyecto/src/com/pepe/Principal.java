15477f3c1c58965e2395955a086ad4ca56bd9b69
rusokverse <rusokverse@MACBOOKPRO>
2022-08-29 21:43:11 -0400

    Primer commit

---
+++ Primer proyecto/src/com/pepe/Principal.java
@@ -0,0 +1,16 @@
+package com.pepe;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.print("Hola mundo");
+		System.out.println("Chau mundo");
+		
+		//Variables
+		int numerito;//definición de variable
+		numerito = 12;//asignación de valor a variable
+		
+		float otroNumero = 3.1416f;
+		double otrito = 3.1416;
+	}
+}
